199986db84e86a61fd7e540c00acbfa8405e7971
sshine <simon@simonshine.dk>
2025-02-08 00:30:01 +0100

    Add rusqlite to server

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -4,5 +4,31 @@ use dioxus::prelude::*;
 /// Echo the user input on the server.
 #[server(Echo)]
 pub async fn echo(input: String) -> Result<String, ServerFnError> {
+    #[cfg(feature = "server")]
+    DB.with(|f| {
+        println!("Saving {} to database.", input);
+        f.execute("INSERT INTO dogs (url) VALUES (?1)", &[&input])
+    })?;
+
     Ok(input)
+}
+
+// The database is only available to server code
+#[cfg(feature = "server")]
+thread_local! {
+    pub static DB: rusqlite::Connection = {
+        // Open the database from the persisted "hotdog.db" file
+        let conn = rusqlite::Connection::open("hotdog.db").expect("Failed to open database");
+
+        // Create the "dogs" table if it doesn't already exist
+        conn.execute_batch(
+            "CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                url TEXT NOT NULL
+            );",
+        ).unwrap();
+
+        // Return the connection
+        conn
+    };
 }
